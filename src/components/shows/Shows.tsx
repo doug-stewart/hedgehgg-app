a9962768e0fc03e18021d4024c7f7aa30b80b449
Doug Stewart <hi@dou.gg>
2026-05-19 12:02:26 -0700

    Migrate to table for shows

--- src/components/shows/Shows.tsx
+++ src/components/shows/Shows.tsx
@@ -1,5 +1,6 @@
 import { format } from "date-fns";
 import { useSonarr } from "../../hooks/useSonarr";
+import styles from "./Shows.module.css";
 
 export const Shows = () => {
   const { upcoming } = useSonarr();
@@ -7,18 +8,35 @@ export const Shows = () => {
   return (
     <section>
       <h2>Upcoming Shows</h2>
-      <ol>
-        {upcoming?.map(({ id, title, series, seasonNumber, episodeNumber, airingAt }) => (
-          <li key={id}>
-            <strong>{title}</strong>
-            <em>{series}</em>
-            <time dateTime={airingAt}>{format(airingAt, "eee, MMM do @ HH:mm")}</time>
-            <span>
-              s{seasonNumber}e{episodeNumber}
-            </span>
-          </li>
-        ))}
-      </ol>
+      <table className={styles.shows}>
+        <thead>
+          <tr>
+            <th>Series</th>
+            <th>Title</th>
+            <th>Airing</th>
+          </tr>
+        </thead>
+        <tbody>
+          {upcoming?.map(
+            ({ id, title, series, seasonNumber, episodeNumber, airingAt }) => (
+              <tr key={id}>
+                <td>{series}</td>
+                <td>
+                  <span>
+                    s{seasonNumber}e{String(episodeNumber).padStart(2, "0")}
+                  </span>
+                  {title}
+                </td>
+                <td>
+                  <time dateTime={airingAt}>
+                    {format(airingAt, "eee, MMM do @ HH:mm")}
+                  </time>
+                </td>
+              </tr>
+            ),
+          )}
+        </tbody>
+      </table>
     </section>
   );
 };
