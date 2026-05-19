05735c6ade47b10aa04a8645e950f1f661037add
Doug Stewart <hi@dou.gg>
2026-05-19 13:26:27 -0700

    Change layout of Shows to be more responsive

--- src/components/shows/Shows.tsx
+++ src/components/shows/Shows.tsx
@@ -6,37 +6,31 @@ export const Shows = () => {
   const { upcoming } = useSonarr();
 
   return (
-    <section>
+    <section className={styles.shows}>
       <h2>Upcoming Shows</h2>
-      <table className={styles.shows}>
-        <thead>
-          <tr>
-            <th>Series</th>
-            <th>Title</th>
-            <th>Airing</th>
-          </tr>
-        </thead>
-        <tbody>
-          {upcoming?.map(
-            ({ id, title, series, seasonNumber, episodeNumber, airingAt }) => (
-              <tr key={id}>
-                <td>{series}</td>
-                <td>
-                  <span>
-                    s{seasonNumber}e{String(episodeNumber).padStart(2, "0")}
-                  </span>
-                  {title}
-                </td>
-                <td>
-                  <time dateTime={airingAt}>
-                    {format(airingAt, "eee, MMM do @ HH:mm")}
-                  </time>
-                </td>
-              </tr>
-            ),
-          )}
-        </tbody>
-      </table>
+      <ul className={styles.list}>
+        {upcoming?.map(
+          ({ id, title, series, seasonNumber, episodeNumber, airingAt }) => (
+            <li className={styles.show} key={id}>
+              <span className={styles.when}>
+                <time
+                  dateTime={airingAt}
+                  title={format(airingAt, "MMM do, HH:mm")}
+                >
+                  {format(airingAt, "eee, HH:mm")}
+                </time>
+              </span>
+              <span className={styles.series}>{series}</span>
+              <span className={styles.episode}>
+                <span className={styles.numbers}>
+                  s{seasonNumber}e{String(episodeNumber).padStart(2, "0")}
+                </span>
+                {title}
+              </span>
+            </li>
+          ),
+        )}
+      </ul>
     </section>
   );
 };
