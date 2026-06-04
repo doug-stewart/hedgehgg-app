725f807b79ccb2ebabd827686b5de615946159e4
Doug Stewart <hi@dou.gg>
2026-06-04 12:39:55 -0700

    Use routes instead of everything living on the app root

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,28 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router";
 import WeatherSprite from "./assets/images/weather-sprite-animated.svg?react";
-import { Hotkeys } from "./components/hotkeys/Hotkeys";
-import { Services } from "./components/services/Services";
-import { Shows } from "./components/shows/Shows";
-import { useSessions } from "./features/auth/hooks/useSession";
-import { Bookmarks } from "./features/bookmarks/components/bookmarks/Bookmarks";
-import { Search } from "./features/search/components/search/Search";
-import { Weather } from "./features/weather/components/weather/Weather";
+import { AuthLayout } from "./features/auth/components/auth-layout/AuthLayout";
+import { User } from "./features/auth/components/user/User";
+import { Home } from "./routes/Home";
+import { Nest } from "./routes/Nest";
+import { Profile } from "./routes/Profile";
 
 const queryClient = new QueryClient();
 
 function App() {
-  const { session, login, signup, logout } = useSessions();
-
   return (
     <>
       <QueryClientProvider client={queryClient}>
-        <h1>Homepa.gg</h1>
-        {session?.user ? (
-          <>
-            <p>{session.user.id}</p>
-            <button onClick={logout} type="button">
-              Logout
-            </button>
-          </>
-        ) : (
-          <>
-            <p>Not logged in</p>
-            <button onClick={() => signup("hi@dou.gg")} type="button">
-              Sign Up
-            </button>
-            <button onClick={login} type="button">
-              Login
-            </button>
-          </>
-        )}
-        <Weather />
-        <Search />
-        <Services />
-        <Shows />
-        <Bookmarks />
-        <Hotkeys />
+        <BrowserRouter>
+          <User />
+          <Routes>
+            <Route element={<Home />} index />
+            <Route element={<AuthLayout />}>
+              <Route element={<Nest />} path="/nest" />
+              <Route element={<Profile />} path="/profile" />
+            </Route>
+          </Routes>
+        </BrowserRouter>
       </QueryClientProvider>
       <WeatherSprite />
     </>
