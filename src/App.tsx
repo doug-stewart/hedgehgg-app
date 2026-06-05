16a2723a93159c280580f477b50d5d735924a9a8
Doug Stewart <hi@dou.gg>
2026-06-04 21:33:58 -0700

    Remove weather icon sprite

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Route, Routes } from "react-router";
-import WeatherSprite from "./assets/images/weather-sprite-animated.svg?react";
 import { AuthLayout } from "./features/auth/components/auth-layout/AuthLayout";
 import { UserHeader } from "./features/auth/components/user-header/UserHeader";
 import { Home } from "./routes/Home";
@@ -11,21 +10,18 @@ const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <UserHeader />
-          <Routes>
-            <Route element={<Home />} index />
-            <Route element={<AuthLayout />}>
-              <Route element={<Nest />} path="/nest" />
-              <Route element={<Profile />} path="/profile" />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-      </QueryClientProvider>
-      <WeatherSprite />
-    </>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <UserHeader />
+        <Routes>
+          <Route element={<Home />} index />
+          <Route element={<AuthLayout />}>
+            <Route element={<Nest />} path="/nest" />
+            <Route element={<Profile />} path="/profile" />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
