e096cfc8eb1583a81f80174665934d1d295e4c1e
Doug Stewart <hi@dou.gg>
2026-06-11 12:02:48 -0700

    Include success check with temporary null return as fallback

--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -2,13 +2,15 @@ import { ProfileForm } from "../features/user/components/profile-form/ProfileFor
 import { useProfile } from "../features/user/hooks/useProfile";
 
 export const Profile = () => {
-  const { profile } = useProfile();
+  const { profile, isSuccess } = useProfile();
 
-  return profile === null ? null : (
+  console.log({ profile });
+
+  return isSuccess ? (
     <>
       <title>Profile • Hedge.gg</title>
       <h1>Profile</h1>
       <ProfileForm profile={profile} />
     </>
-  );
+  ) : null;
 };
