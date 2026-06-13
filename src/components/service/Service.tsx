76bec9b720ed9b2d9e68495d89c015066ea676f8
Doug Stewart <hi@dou.gg>
2026-06-13 13:53:03 -0700

    Add basic tooltip over services

--- src/components/service/Service.tsx
+++ src/components/service/Service.tsx
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import type { Service as TService } from "../../types";
 
 import styles from "./Service.module.css";
@@ -7,11 +8,36 @@ type ServiceProps = {
 };
 
 export const Service = ({ service }: ServiceProps) => {
-  const { name, href, icon } = service;
+  const tooltip = useRef<HTMLSpanElement>(null);
+  const { name, href, icon, abbr } = service;
+
+  const toggleTooltip = () => {
+    tooltip?.current?.togglePopover();
+  };
+
   return (
     <li className={styles.service}>
-      <a className={styles.link} href={href} rel="noopener noreferrer" title={name}>
+      <a
+        className={styles.link}
+        href={href}
+        onMouseEnter={toggleTooltip}
+        onMouseLeave={toggleTooltip}
+        popoverTarget={`service-${abbr}`}
+        popoverTargetAction="toggle"
+        rel="noopener noreferrer"
+      >
         <img alt={`${name} icon`} className={styles.icon} src={icon} />
+
+        <span
+          className={styles.tootlip}
+          id={`service-${abbr}`}
+          popover="hint"
+          ref={tooltip}
+          role="tooltip"
+        >
+          {name}
+          <span className={styles.arrow} />
+        </span>
       </a>
     </li>
   );
