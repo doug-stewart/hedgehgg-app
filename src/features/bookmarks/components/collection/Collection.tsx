3ddfcec418222d77657b3dea872991bb72de5aec
Doug Stewart <hi@dou.gg>
2026-06-13 14:04:53 -0700

    Filter links based on search

--- src/features/bookmarks/components/collection/Collection.tsx
+++ src/features/bookmarks/components/collection/Collection.tsx
@@ -1,3 +1,4 @@
+import { useSearchStore } from "../../../search/stores/search.store";
 import type { Collection as TCollection } from "../../types";
 import { Link } from "../link/Link";
 
@@ -7,15 +8,18 @@ type CollectionProps = { collection: TCollection };
 
 export const Collection = ({ collection }: CollectionProps) => {
   const { id, name, links } = collection;
+  const searchFilter = useSearchStore((state) => state.filter);
+  const filteredLinks = links.filter((link) =>
+    link.name.toLowerCase().includes(searchFilter.toLowerCase()),
+  );
+
   return (
     <li key={id}>
       <h3>{name}</h3>
       <ul className={styles.links}>
-        {links
-          .sort((a, b) => a.name.localeCompare(b.name))
-          .map((link) => (
-            <Link key={link.id} link={link} />
-          ))}
+        {filteredLinks.map((link) => (
+          <Link key={link.id} link={link} />
+        ))}
       </ul>
     </li>
   );
