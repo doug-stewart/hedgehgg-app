b40a09e37b882e648f2ed4e89745fbb728334601
Doug Stewart <hi@dou.gg>
2026-07-25 15:10:42 -0700

    Make sure we default to sending credentials

--- src/api/helpers/apiFetch.ts
+++ src/api/helpers/apiFetch.ts
@@ -1,12 +1,6 @@
 import { BACKEND_API } from "@/config";
 
-export const apiFetch = (
-  path: string,
-  options?: RequestInit | null,
-  includeCredentials?: boolean,
-) => {
-  return fetch(
-    `${BACKEND_API}/api${path}`,
-    Object.assign({}, options, includeCredentials ? { credentials: "include" } : null),
-  );
+export const apiFetch = (path: string, options?: RequestInit | null, includeCredentials = true) => {
+  const opts = Object.assign({}, options, includeCredentials ? { credentials: "include" } : null);
+  return fetch(`${BACKEND_API}/api${path}`, opts);
 };
