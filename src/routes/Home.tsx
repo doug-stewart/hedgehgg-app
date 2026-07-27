48488b0c7ad612d9ed453dbb59dac02b3d2bf535
Doug Stewart <hi@dou.gg>
2026-07-27 15:51:39 -0700

    Naming things is hard.

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,18 +1,21 @@
 import Hedgehgg from "@/assets/images/hedgehgg.svg?react";
 import { SessionForm } from "@/features/auth/components/session-form/SessionForm";
 import { SessionWatcher } from "@/features/auth/components/session-watcher/SessionWatcher";
+import styles from "./Home.module.css";
 
 export default function Home() {
   return (
     <>
-      <title>Hedge.gg</title>
-      <header>
-        <Hedgehgg role="presentation" />
-        <h1>Hedge.hgg</h1>
-        <p>Build your nest!</p>
-      </header>
-      <SessionForm />
-      <SessionWatcher />
+      <title>Hedgeh.gg</title>
+      <div className={styles.wrapper}>
+        <header className={styles.header}>
+          <Hedgehgg role="presentation" />
+          <h1>Hedgeh.gg</h1>
+          <p>Build your nest!</p>
+        </header>
+        <SessionForm />
+        <SessionWatcher />
+      </div>
     </>
   );
 }
